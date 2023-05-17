52a5b5e92d2968dc76b39e3827e926618e4f4dc1
Noah Greer <noahjgreer@gmail.com>
2023-05-17 13:07:31 -0700

    v0.0.2
    + Created Mockup Login Screen

---
+++ assets/basics.js
@@ -0,0 +1,5 @@
+// console.log(document.getElementsByClassName('img').item);
+
+// document.getElementsByClassName('img').item.setAttribute('draggable', false);
+
+// .setAttribute('draggable', false);
