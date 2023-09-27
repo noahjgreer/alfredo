1904510b487a1be9703c964c987a2270da45be5d
Noah Greer <noahjgreer@gmail.com>
2023-09-27 15:30:57 -0700

    + Made more minor changes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 
 const app = express();
 const fs = require('fs');
+const fsp = require('fs/promises');
+const { json } = require('body-parser');
 const port = 3000;
 const httpsPort = 3001;
 
@@ -26,14 +28,47 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    console.log(req.body);
-    // res.write(JSON.stringify({
-    //response: "I recieved the request. Just letting you know. I don't know how to pass through a message simply without encoding it into JSON, so I hope you don't mind. I mean, I am sure I could probably figure it out if I gave it the time but I am tired and should probably head to bed now uwu."
-//}));
-    res.writeHead(418);
+    var response;
+    checkDatabaseForCreds(req.body.username, req.body.password).then((feedback) => {
+        console.log(feedback);
+        // Search Feedback for username and password
+        for (var i = 0; i < feedback.users.length; i++) {
+            console.log(req.body.name + feedback.users[i].name + req.body.pass + feedback.users[i].password);
+            if (req.body.name == feedback.users[i].name && req.body.pass == feedback.users[i].password) {
+                console.log("Valid user!" + feedback.users[i].name);
+
+                assignToken(i);
+                break;
+            } else {
+                console.log("You are not logged in.");
+            }
+        }
+    });
+    console.log(req.body.name);
+    console.log(response);
+    res.writeHead(200);
+    res.write(JSON.stringify({
+    response: "I recieved the request. Just letting you know. I don't know how to pass through a message simply without encoding it into JSON, so I hope you don't mind. I mean, I am sure I could probably figure it out if I gave it the time but I am tired and should probably head to bed now uwu."
+}));
     res.end();
 })
 
+async function checkDatabaseForCreds(username, password) {
+    try {
+        const data = await fsp.readFile('Z:/Web/Private/alfredo/users.json', 'utf8');
+        const database = JSON.parse(data);
+        return database;
+    } catch (err) {
+        console.error(err + " Uh oh, stinky!");
+    }
+}
+
+async function assignToken(userLocation) {
+}
+
+
+
+// Server Starting
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer({
     key: fs.readFileSync("Z:/Web/MuffinMode/certificates/alfredo/privkey1.pem"),
@@ -47,44 +82,3 @@ httpServer.listen(port, () => {
 httpsServer.listen(httpsPort, () => {
     console.log('HTTPS server running on port ' + httpsPort);
 })
-
-/*const express = require('express')
-const app = express()
-const port = 3000
-
-
-app.get('/', (req, res) => {
-    // Accepting the Request
-    console.log('ACCEPTED request to: ' + req.url + ', at: ' + new Date().toISOString());
-    res.sendStatus(202);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    // Checking credentials
-    if (req.headers['credentials'] == "alfredo") {
-        console.log('CREDENTIALS MATCHED.');
-        res.send(JSON.stringify(req.headers['x-api-key']));
-        res.end(() => {
-            console.log('ENDED response to: ' + req.url + ', at: ' + new Date().toISOString());
-        });
-        setTimeout(() => {
-            res.connection.destroy('504')
-        }, 5000)
-    } else {
-        console.log('API KEY MISMATCH: ' + req.headers['credentials'] + " with headers: " + JSON.stringify(req.headers));
-        console.log('ENDED response to: ' + req.url + ', at: ' + new Date().toISOString());
-        res.connection.destroy('401');
-    }
-    console.log('Request Recieved.');
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
-
-function closeSockets() {
-    app.connection.destroy();
-}*/
