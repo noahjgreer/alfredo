a8d689efa9ecf9362939e33bb8faba342901f740
Noah Greer <noahjgreer@gmail.com>
2023-11-06 19:52:35 -0800

    v0.0.16-1
    + Added official dropdown arrow pair
    + Fixed a debug footprint

--- assets/scripts/application.js
+++ assets/scripts/application.js
@@ -13,7 +13,7 @@ let listableSections = ['tasks', 'routines', 'events'];
 
 // Fetch tasks from default section on page load
 async function init() {
-    let taskBody = await loadSection('new');
+    let taskBody = await loadSection('tasks');
     // fetchFromCategory('tasks', undefined, taskBody, true);
 }
 
